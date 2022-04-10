ef866a1414a13a8356830f771f01f6383e979564
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-04-10 17:22:50 -0300

    feat: create mutation createCustomer

--- src/http/graphql/resolvers/customers.resolver.ts
+++ src/http/graphql/resolvers/customers.resolver.ts
@@ -1,11 +1,12 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { CustomersService } from '../../../services/customers.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { AuthUser, CurrentUser } from '../../auth/currentUser';
 
 import { Customer } from '../models/customer';
+import { CreateCustomerInput } from '../inputs/createCustomerInput';
 
 @Resolver(() => Customer)
 export class CustomersResolver {
@@ -18,6 +19,18 @@ export class CustomersResolver {
     user: AuthUser,
   ) {
     const customer = await this.customersService.findByUserId(user.sub);
+
+    return customer;
+  }
+
+  // TODO: verify if authenticated user has admin role (only admin can create customer)
+  @Mutation(() => Customer)
+  @UseGuards(AuthorizationGuard)
+  async createCustomer(
+    @Args('data')
+    data: CreateCustomerInput,
+  ) {
+    const customer = await this.customersService.create(data);
 
     return customer;
   }
--- src/services/customers.service.ts
+++ src/services/customers.service.ts
@@ -2,9 +2,29 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface CreateCustomerParams {
+  userId: string;
+}
+
 @Injectable()
 export class CustomersService {
   constructor(private prisma: PrismaService) {}
+
+  async create({ userId }: CreateCustomerParams) {
+    const customer = await this.prisma.customer.findUnique({
+      where: { userId },
+    });
+
+    if (customer) {
+      throw new Error('Customer already exists!');
+    }
+
+    return this.prisma.customer.create({
+      data: {
+        userId,
+      },
+    });
+  }
 
   async findByUserId(userId: string) {
     const customer = await this.prisma.customer.findUnique({
